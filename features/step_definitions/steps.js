b1a0eaf698b6127ff1c6663882ea77a61a294961
Federico Iachetti <iachetti.federico@gmail.com>
2020-06-03 11:33:20 -0300

    Replace hardcoded sean instance

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -12,13 +12,8 @@ Given('a person named {word}', function (name) {
   this.people[name] = new Person(this.network)
 })
 
-Given('a person named Sean', function () {
-  this.sean = new Person(this.network)
-})
-
 When('Sean shouts {string}', function (message) {
-  sean = new Person(this.network)
-  sean.shout(message)
+  this.people['Sean'].shout(message)
   this.messageFromSean = message
 })
 
