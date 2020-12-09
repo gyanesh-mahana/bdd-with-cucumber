4b2f4bdcdc0f6107f44b2c02210412ed170545f4
gyanesh-mahana <gkm010@gmail.com>
2020-12-09 17:21:18 +0530

    updated till Chapter-4:Cleaning up https://school.cucumber.io/courses/take/bdd-with-cucumber-java/quizzes/14724042-knowledge-check-4-6

--- shouty/src/test/java/shouty/StepDefinitions.java
+++ shouty/src/test/java/shouty/StepDefinitions.java
@@ -1,34 +1,49 @@
 package shouty;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+//import java.util.ArrayList;
+import java.util.HashMap;
 import static java.util.Arrays.asList;
+
 import static org.junit.Assert.assertEquals;
+
 
 public class StepDefinitions {
 
-    private Person sean;
+    private Person person;
     private Person lucy;
-    private String messageFromSean;
-
-    @Given("Lucy is {int} metres from Sean")
-    public void lucy_is_located_m_from_Sean(int distance) throws Throwable {
-        Network network = new Network();
-        sean = new Person(network);
-        lucy = new Person(network);
-        lucy.moveTo(distance);
+    private String messageFromPerson;
+    private Network network;
+    //private Network network = new Network();
+    private HashMap<String, Person> personMap;
+    
+    //@Before hook to replace 
+    @Before
+    public void createNetwork() {
+    	network = new Network(); 
+    	personMap = new HashMap<String, Person>();
+    	//messageFromPerson = new ArrayList<String>();
+    }
+    
+    @Given("a person named {word}")
+    public void a_person_named(String name) throws Throwable {
+        //create a person named Lucy
+    	personMap.put(name, new Person(network));
     }
 
-    @When("Sean shouts {string}")
-    public void sean_shouts(String message) throws Throwable {
-        sean.shout(message);
-        messageFromSean = message;
+    @When("{word} shouts {string}")
+    public void person_shouts(String name, String message) throws Throwable {
+        personMap.get(name).shout(message);
+        //messageFromPerson.add(message);
+        messageFromPerson = message;
     }
 
-    @Then("Lucy should hear Sean's message")
-    public void lucy_hears_Sean_s_message() throws Throwable {
-        assertEquals(asList(messageFromSean), lucy.getMessagesHeard());
+    @Then("{word} should hear {word}'s message")
+    public void lucy_hears_Sean_s_message(String name1, String name2) throws Throwable {
+        assertEquals(asList(messageFromPerson), personMap.get(name1).getMessagesHeard());
     }
 }
--- shouty/src/test/resources/shouty/hear_shout.feature
+++ shouty/src/test/resources/shouty/hear_shout.feature
@@ -1,11 +1,39 @@
+
 Feature: Shout
 
-  Scenario: Listener within range
-    Given Lucy is 15 metres from Sean
-    When Sean shouts "free bagels at Sean's"
-    Then Lucy should hear Sean's message
-
-  Scenario: Listener hears a different mesage
-    Given Lucy is 15 metres from Sean
-    When Sean shouts "Free coffee!"
-    Then Lucy should hear Sean's message
+	Description-
+		This is a piece in feature file which is not a part of gherkin
+		This is just for the purpose of writing important comments or to-do lost etc.
+	
+	Shouty allows users to "hear" other users "shouts" when they are close enough
+	
+	To do:
+		-only shout to people within a certain distance
+	
+	Tips:
+		-Business Rules are defines with Rule tag
+		-Backgrounds a
+			-Used to avoid repeated contexts and to improve readability, also they are written before the Business rules are defined
+			-Sometimes they can reduce readability and maintainability
+		
+	Background:    
+			Given a person named Lucy
+	    And a person named Sean  	
+	
+	Rule: Shouts can be heard from other users
+			
+	  Scenario: Listener hears the message
+	    When Sean shouts "free bagels at Sean's"
+	    Then Lucy should hear Sean's message
+	
+	  Scenario: Listener hears a different message
+	    When Sean shouts "Free coffee!"
+	    Then Lucy should hear Sean's message
+	    
+	Rule: Shouts should only be heard to listener within range
+	
+		Scenario: Listener is within range
+		
+		Scenario: Listener is out of range
+		
+		
