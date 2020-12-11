ac3c78c76ed264bc54cc99c53af3dda44bb90f9b
gyanesh-mahana <gkm010@gmail.com>
2020-12-11 11:02:53 +0530

    location feature added to Person Class

--- shouty/src/main/java/shouty/Person.java
+++ shouty/src/main/java/shouty/Person.java
@@ -4,23 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Person {
-	private final List<String> messagesHeard = new ArrayList<String>();
-	private final Network network;
+    private final List<String> messagesHeard = new ArrayList<String>();
+    private final Network network;
+    private final int location;
 
-	public Person(Network network) {
-		this.network = network;
-		network.subscribe(this);
-	}
+    public Person(Network network, int location) {
+        this.network = network;
+        this.location = location; //location added
+        network.subscribe(this);
+    }
 
-	public List<String> getMessagesHeard() {
-		return messagesHeard;
-	}
+    public List<String> getMessagesHeard() {
+        return messagesHeard;
+    }
 
-	public void shout(String message) {
-		network.broadcast(message);
-	}
+    public void shout(String message) {
+        network.broadcast(message, getLocation());
+    }
 
-	public void hear(String message) {
-		messagesHeard.add(message);
-	}
+    public void hear(String message) {
+        messagesHeard.add(message);
+    }
+
+    public int getLocation() {
+        return location;
+    }
 }
