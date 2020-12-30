8a102d5111374f6c75be8ff07b3557e4186d1af5
gyanesh-mahana <gkm010@gmail.com>
2020-12-30 16:31:02 +0530

    MINOR CHANGES(revision)

--- shouty/src/test/java/shouty/StepDefinitions.java
+++ shouty/src/test/java/shouty/StepDefinitions.java
@@ -1,12 +1,16 @@
 package shouty;
 
 import io.cucumber.java.Before;
+import io.cucumber.java.DataTableType;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 //import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static java.util.Arrays.asList;
 
 import static org.junit.Assert.assertEquals;
@@ -14,39 +18,37 @@ import static org.junit.Assert.assertNotEquals;
 
 public class StepDefinitions {
 
-//	private Person person;
-//	private Person lucy;
 	private String messageFromShouter;
 	private Network network;
 	// private Network network = new Network();
 	private HashMap<String, Person> personMap;
 
+	// Whereabout Class (not a part of business rule)
+	// for internal use in testing only
+	static class Whereabouts {
+		public String name;
+		public Integer location;
+
+		public Whereabouts(String name, Integer location) {
+			this.name = name;
+			this.location = location;
+		}
+	}
+
+	// DataTableType hook is used to transform the DataTable into desired Class type
+	@DataTableType
+	public Whereabouts definewhereabouts(Map<String, String> entry) {
+		return new Whereabouts(entry.get("name"), Integer.parseInt(entry.get("location")));
+	}
+
 	// @Before hook to replace
 	@Before
 	public void createNetwork() {
-		//network = new Network(0);
+		// network = new Network(0);
 		personMap = new HashMap<String, Person>();
 		// messageFromPerson = new ArrayList<String>();
 	}
 
-//	@Given("a person named {word}")
-//	public void a_person_named(String name) throws Throwable {
-//		// create a person named Lucy
-//		personMap.put(name, new Person(network, 0));
-//	}
-
-	@When("{word} shouts {string}")
-	public void person_shouts(String name, String message) throws Throwable {
-		personMap.get(name).shout(message);
-		// messageFromPerson.add(message);
-		messageFromShouter = message;
-	}
-
-	@Then("{word} should hear {word}'s message")
-	public void listener_hears_shouter_message(String listenerName, String shouterName) throws Throwable {
-		assertEquals(asList(messageFromShouter), personMap.get(listenerName).getMessagesHeard());
-	}
-	
 	@Given("the range is {int}")
 	public void the_range_is(Integer distance) throws Throwable {
 		network = new Network(distance);
@@ -56,11 +58,41 @@ public class StepDefinitions {
 	public void a_person_named_is_located_at(String name, Integer location) throws Throwable {
 		personMap.put(name, new Person(network, location));
 	}
-	
-	@Then("{word} should not hear {word}'s message")
-	public void listener_should_not_hear_shouter_message(String listenerName, String shouterName) throws Throwable {
-		assertNotEquals(asList(messageFromShouter), personMap.get(listenerName).getMessagesHeard());
-		
+
+	@Given("persons are located at")
+	public void persons_are_located(List<Whereabouts> whereabouts) {
+
+//		io.cucumber.datatable.DataTable dataTable
+//		//Way 1: Using Cell data 
+//		personMap.put(dataTable.cell(1, 0), new Person(network, Integer.parseInt(dataTable.cell(1, 1))));
+//		personMap.put(dataTable.cell(2, 0), new Person(network, Integer.parseInt(dataTable.cell(2, 1))));
+
+//		//Way 2: Using Maps
+//		for(Map<String, String> person : dataTable.asMaps()) {
+//			personMap.put(person.get("name"), new Person(network, Integer.parseInt(person.get("location"))));
+//		}
+
+		// Way 3: After transforming data into Person object
+		for (Whereabouts whereabout : whereabouts) {
+			personMap.put(whereabout.name, new Person(network, whereabout.location));
+		}
 	}
 
+	@When("{word} shouts {string}")
+	public void person_shouts(String name, String message) throws Throwable {
+		personMap.get(name).shout(message);
+		// messageFromPerson.add(message);
+		messageFromShouter = message;
+	}
+
+	@Then("{word} should hear {word}('s) message")
+	public void listener_hears_shouter_message(String listenerName, String shouterName) throws Throwable {
+		assertEquals(asList(messageFromShouter), personMap.get(listenerName).getMessagesHeard());
+	}
+
+	@Then("{word} should not hear {word}('s) message")
+	public void listener_should_not_hear_shouter_message(String listenerName, String shouterName) throws Throwable {
+		assertNotEquals(asList(messageFromShouter), personMap.get(listenerName).getMessagesHeard());
+
+	}
 }
--- shouty/src/test/resources/shouty/hear_shout.feature
+++ shouty/src/test/resources/shouty/hear_shout.feature
@@ -1,43 +1,45 @@
 Feature: Shout
+  Description-
+  This is a piece in feature file which is not a part of gherkin
+  This is just for the purpose of writing important comments or to-do lost etc.
+  
+  Shouty allows users to "hear" other users "shouts" when they are close enough
+  Rule keyword is used
+  To do:
+  -Data table implementation
+  
+  Tips:
+  -Business Rules are defines with Rule tag, after Gherkin V6 (here shows Suntax error :()
+  -Backgrounds a
+  	-Used to avoid repeated contexts and to improve readability, also they are written before the Business rules are defined
+  	-Sometimes they can reduce readability and maintainability
+  -Table can be used to represent data with pipe(|) separators, gherkin removes the spaces trailing |
 
-	Description-
-		This is a piece in feature file which is not a part of gherkin
-		This is just for the purpose of writing important comments or to-do lost etc.
-	
-	Shouty allows users to "hear" other users "shouts" when they are close enough
-	
-	To do:
-		-only shout to people within a certain distance
-	
-	Tips:
-		-Business Rules are defines with Rule tag
-		-Backgrounds a
-			-Used to avoid repeated contexts and to improve readability, also they are written before the Business rules are defined
-			-Sometimes they can reduce readability and maintainability	
-	
-	Rule: Shouts can be heard from other users
-			
-	  Scenario: Listener hears the message
-	  	Given the range is 100
-	  	And a person named Lucy is located at 0
-	    And a person named Sean is located at 50
-	    When Sean shouts "free bagels at Sean's"
-	    Then Lucy should hear Sean's message
-	    
-	Rule:	Shouts can only be heard within range
-	    
-		Scenario: Listener is within range
-			Given the range is 100
-			And a person named Lucy is located at 0
-	    And a person named Sean is located at 50
-	    When Sean shouts "free bagels at Sean's"
-	    Then Lucy should hear Sean's message
-		
-		Scenario: Listener is out of range
-			Given the range is 100
-			And a person named Larry is located at 0
-	    And a person named Mohan is located at 120
-	    When Mohan shouts "free bagels at Mohan's"
-	    Then Larry should not hear Mohan's message
+  #Rule: Shouts can be heard from other users
+  Scenario: Listener hears the message
+    Given the range is 100
+    And persons are located at
+      | name | location |
+      | Lucy |        0 |
+      | Sean |       50 |
+    When Sean shouts "free bagels at Sean's"
+    Then Lucy should hear Sean's message
 
-		
+  #Rule:	Shouts can only be heard within range
+  Scenario: Listener is within range
+    Given the range is 100
+    And persons are located at
+      | name | location |
+      | Lucy |        0 |
+      | Sean |       50 |
+    When Sean shouts "free bagels at Sean's"
+    Then Lucy should hear Sean's message
+
+  Scenario: Listener is out of range
+    Given the range is 100
+    And persons are located at
+      | name  | location |
+      | Larry |        0 |
+      | Mohan |      150 |
+    When Mohan shouts "free bagels at Mohan's"
+    Then Larry should not hear Mohan message
